0f4ef409d3bb701deca0a5be375164464d343e13
Cory Barker <barkercbel@gmail.com>
2017-03-30 20:57:49 -0600

    .

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ApplicationRecord
   
-  has_attached_file :image, styles: { large: "600x600>", thumb: "150x150#" }, s3_protocol: :https, s3_host_name: :s3-us-west-2.amazonaws.com, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "600x600>", thumb: "150x150#" }, s3_protocol: :https, s3_host_name: 's3-us-west-2.amazonaws.com', default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   
 end
